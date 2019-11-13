c4c90316bed72f5486676b11c68052b7046b2521
John Wika Haakseth <johnwika@gmail.com>
2019-11-13 21:23:38 +0100

    Updates

---
+++ precache-manifest.9380bd4b58a68d55963255925b79d9bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1b2e50ab3b1033b424d1c5483b52de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68940c6ef62b9b612cbd",
+    "url": "/static/css/2.e71d8db5.chunk.css"
+  },
+  {
+    "revision": "7ede18af9b2cf6810ab2",
+    "url": "/static/css/main.11a4e593.chunk.css"
+  },
+  {
+    "revision": "68940c6ef62b9b612cbd",
+    "url": "/static/js/2.599b0e0a.chunk.js"
+  },
+  {
+    "revision": "7ede18af9b2cf6810ab2",
+    "url": "/static/js/main.72c00f23.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
